2bc0285d4fa9e9cf47b4c7c6cd7ad41f9c7e44c9
Juan <72490350+jusidama18@users.noreply.github.com>
2021-03-14 22:37:50 +0700

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -145,7 +145,7 @@ def searchhelp(update, context):
 • /sts <i>[search query]</i>
 • /sukebei <i>[search query]</i>
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_photo("https://f.cyberdrop.cc/prrgsr-4KHsJD3j.png", help_string, parse_mode=ParseMode.HTML)
     
     
 SEARCHHELP_HANDLER = CommandHandler("helptorrent", searchhelp)
