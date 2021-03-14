ad7d174217c1935d576504fd5897a7deaad3fb57
Juan <72490350+jusidama18@users.noreply.github.com>
2021-03-14 15:07:43 +0700

    Update gdriveToolz.py

--- bot/helper/mirror_utils/upload_utils/gdriveToolz.py
+++ bot/helper/mirror_utils/upload_utils/gdriveToolz.py
@@ -11,9 +11,7 @@ from googleapiclient.errors import HttpError
 
 from telegram import InlineKeyboardMarkup
 from bot.helper.telegram_helper import button_builder
-from bot import DRIVE_NAME, DRIVE_ID, INDEX_URL, telegra_ph, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, SHORTENER, SHORTENER_API
-from bot.helper.ext_utils.bot_utils import *
-from bot.helper.ext_utils.fs_utils import get_mime_type, get_path_size
+from bot import DRIVE_NAME, DRIVE_ID, INDEX_URL, telegra_ph
 
 LOGGER = logging.getLogger(__name__)
 logging.getLogger('googleapiclient.discovery').setLevel(logging.ERROR)
@@ -41,7 +39,7 @@ class GoogleDriveHelper:
         try:
             return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
         except IndexError:
-            return 'File too large'
+            return 'File too large 😱'
 
     def authorize(self):
         # Get credentials
@@ -89,9 +87,7 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             telegra_ph.edit_page(path = self.path[prev_page],
-                                 title = 'John Torrent v.2.0 Bot Search',
-                                 author_name='Jusidama',
-                                 author_url='https://t.me/jusidama',
+                                 title = '⚡️ Database Index | Search Results',
                                  html_content=content)
         return
 
@@ -105,72 +101,46 @@ class GoogleDriveHelper:
             INDEX += 1          
             if response:
                 if add_title_msg == True:
-                    msg = f'<h3>Search Results for : {fileName}</h3><br>@LoaderXbot #ProjektX<br><br>'
+                    msg = f'<h3><img src="https://f.cyberdrop.cc/image-removebg-preview%20(1)-JUhZCiMc.png" alt="Paris" class="center"><br>🔎 Search Results for : {fileName}</h3><br><b><a href="https://t.me/jusidama">@Jusidama #Search-Index</a></b><br><br><b><a href="https://groups.google.com/g/jusidama-folder">Join Our Team Drive</a> | <a href="https://index.juicedama.workers.dev">Index Multi Drive</a></b></br><br><br>'
                     add_title_msg = False
-                msg += f"╾────────────╼<br><b>{DRIVE_NAME[INDEX]}</b><br>╾────────────╼<br>"
-                for file in response.get('files', []):
-                if file.get('mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
-                    furl = f"https://drive.google.com/drive/folders/{file.get('id')}"
-                    msg += f"⁍<code>{file.get('name')}<br>(📁 Folder)</code><br>"
-                    if SHORTENER is not None and SHORTENER_API is not None:
-                        sfurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, furl)).text
-                        msg += f"<b><a href={sfurl}>☁️ Drive Link</a></b>"
+                msg += f"╾────────────╼<br><b>👑 {DRIVE_NAME[INDEX]} 👑</b><br>╾────────────╼<br>"
+                for file in response:
+                    if file.get('mimeType') == "application/vnd.google-apps.folder":  # Detect Whether Current Entity is a Folder or File.
+                        msg += f"⚜️ <code>{file.get('name')}</code> <b>(📁 Folder)</b><br>" \
+                               f"<b><a href='https://drive.google.com/drive/folders/{file.get('id')}'>☁️ Drive Link</a></b>"
+                        if INDEX_URL[INDEX] is not None:
+                            url_path = requests.utils.quote(f'{file.get("name")}')
+                            url = f'{INDEX_URL[INDEX]}/{url_path}/'
+                            msg += f'<b> | <a href="{url}">🔗 Index Link</a></b>'
                     else:
-                        msg += f"<b><a href={furl}>☁️ Drive Link</a></b>"
-                    if INDEX_URL is not None:
-                        url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}/')
-                        if SHORTENER is not None and SHORTENER_API is not None:
-                            siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                            msg += f' <b>| <a href="{siurl}">🔗 Index Link</a></b>'
-                        else:
-                            msg += f' <b>| <a href="{url}">🔗 Index Link</a></b>'
-                    if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
-                       buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
-                                                         
-                else:
-                    furl = f"https://drive.google.com/uc?id={file.get('id')}&export=download"
-                    msg += f"⁍<code>{file.get('name')}<br>({get_readable_file_size(int(file.get('size')))})📄</code><br>"
-                    if SHORTENER is not None and SHORTENER_API is not None:
-                        sfurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, furl)).text
-                        msg += f"<b><a href={sfurl}>☁️ Drive Link</a></b>"
-                    else:
-                        msg += f"<b><a href={furl}>☁️ Drive Link</a></b>"
-                    if INDEX_URL is not None:
-                        url = requests.utils.requote_uri(f'{INDEX_URL}/{file.get("name")}')
-                        if SHORTENER is not None and SHORTENER_API is not None:
-                            siurl = requests.get('https://{}/api?api={}&url={}&format=text'.format(SHORTENER, SHORTENER_API, url)).text
-                            msg += f' <b>| <a href="{siurl}">🔗 Index Link</a></b>'
-                        else:
-                            msg += f' <b>| <a href="{url}">🔗 Index Link</a></b>'
-                    if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
-                       buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
-                msg += '<br><br>'
-                content_count += 1
-                if content_count == TELEGRAPHLIMIT :
-                    self.telegraph_content.append(msg)
-                    msg = ""
-                    content_count = 0
+                        msg += f"⚜️ <code>{file.get('name')}</code> <br><b>(📄 {self.get_readable_file_size(file.get('size'))})</b><br>" \
+                               f"<b><a href='https://drive.google.com/uc?id={file.get('id')}&export=download'>☁️ Drive Link</a></b>"
+                        if INDEX_URL[INDEX] is not None:
+                            url_path = requests.utils.quote(f'{file.get("name")}')
+                            url = f'{INDEX_URL[INDEX]}/{url_path}'
+                            msg += f'<b> | <a href="{url}">🔗 Index Link</a></b>'
+                    msg += '<br><br>'
+                    content_count += 1
+                    if content_count == TELEGRAPHLIMIT :
+                       self.telegraph_content.append(msg)
+                       msg = ""
+                       content_count = 0
 
-            if msg != '':
-                self.telegraph_content.append(msg)
+        if msg != '':
+            self.telegraph_content.append(msg)
 
-            if len(self.telegraph_content) == 0:
-                return "No result found.. 😞", None
+        if len(self.telegraph_content) == 0:
+            return "No result found.. 😞", None
 
-            for content in self.telegraph_content :
-                self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'John Torrent v.2.0 Bot Search',
-                                                        author_name='Jusidama',
-                                                        author_url='https://t.me/jusidama',
-                                                        html_content=content
-                                                        )['path'])
+        for content in self.telegraph_content :
+            self.path.append(telegra_ph.create_page(title = '⚡️ Database Index | Search Results',author_name='Jusidama',author_url='https://t.me/jusidama',html_content=content )['path'])
 
-            self.num_of_path = len(self.path)
-            if self.num_of_path > 1:
-                self.edit_telegraph()
+        self.num_of_path = len(self.path)      
+        if self.num_of_path > 1:
+            self.edit_telegraph()
 
-            msg = f"<b>Search Results For {fileName} 👇🏻 </b>"
-            buttons = button_build.ButtonMaker()   
-            buttons.buildbutton("👉🏻 Click Here 👈🏻", f"https://telegra.ph/{self.path[0]}")
+        msg = f" Search Results For {fileName} 👇🏻 "
+        buttons = button_builder.ButtonMaker()   
+        buttons.buildbutton("👉🏻 Click Here 👈🏻", f"https://telegra.ph/{self.path[0]}")
 
         return msg, InlineKeyboardMarkup(buttons.build_menu(1))
