00ef19e46dd624369169a9aa3a2f81ee09716a95
Juan <72490350+jusidama18@users.noreply.github.com>
2021-03-17 00:32:41 +0700

    Update sample_config.py

--- bot/sample_config.py
+++ bot/sample_config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    APP_ID = int(os.environ.get("TELEGRAM_ID", 12345))
-    API_HASH = os.environ.get("TELEGRAM_HASH")
+    TELEGRAM_ID = int(os.environ.get("TELEGRAM_ID", 12345))
+    TELEGRAM_HASH = os.environ.get("TELEGRAM_HASH")
     OWNER_ID = int(os.environ.get("OWNER_ID", 1303747114))
     AUTH_CHANNEL = set(int(x)
                        for x in os.environ.get("AUTH_CHANNEL", "-1001221644423").split())
