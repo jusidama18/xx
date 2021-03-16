fe8257f55748eca10849e90f210076d576acfc5a
Juan <72490350+jusidama18@users.noreply.github.com>
2021-03-17 00:10:06 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,9 +4,6 @@ from bot.g_config import Config
 
 #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
 
-#class Config(Config):
-    TG_BOT_TOKEN= "1424599373:AAE7KhWEy0AcDck48OvjwXkYyNq-SH9jiLk"
-    APP_ID = 1889596
-    API_HASH = "e4da326b521ad1cf5d864365f582c8fd"
+class Config(Config):
     OWNER_ID = 1303747114
     AUTH_CHANNEL = [-1001221644423]
