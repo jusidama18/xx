1821ed2104b54b9eacc815cb4f055e90a7c9a668
Juan <72490350+jusidama18@users.noreply.github.com>
2021-05-04 17:41:31 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -22,5 +22,10 @@ class _BotCommands:
         self.DeleteCommand = 'del'
         self.UsageCommand = 'usage'
         self.AuthorizedUsersCommand = 'users'
+	    self.SystemstatsCommand= 'system'
+        self.WikiCommand= 'wiki'
+        self.TotranslateCommand = 'tr'
+        self.PasteCommand = 'paste'
+        self.systemkutestCommand = 'neofetch'
         
 BotCommands = _BotCommands()
